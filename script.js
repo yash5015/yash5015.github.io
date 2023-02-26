aa533f646f13df9081603f98ed52f9e6441c924d
yash5015 <yashrathor5015@gmail.com>
2023-02-26 14:00:43 +0530

    update feb23

--- script.js
+++ script.js
@@ -29,6 +29,10 @@ window.addEventListener("scroll", function () {
   const nav = document.querySelector("nav");
   nav.classList.toggle("sticky", window.scrollY > 0);
 });
+window.addEventListener("scroll", function () {
+  const gototop_btn = document.querySelector(".gototop");
+  gototop_btn.classList.toggle("gototop-show", window.scrollY > 100);
+});
 
 function toggleMenu() {
   const menuToggle = document.querySelector(".menuToggle");
